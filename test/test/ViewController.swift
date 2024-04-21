88ec4f94a28af40a2453243f6d984cb585f0d846
shadon2024 <in-con23@yandex.ru>
2024-04-22 00:31:35 +0500

    new color, new info

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var name = "name"
+    var surName = "surname"
+    var lastname = "lastname"
+    var info: String?
+    var user: String = "user"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
