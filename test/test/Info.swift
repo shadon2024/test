77a363e67c868b7793c795ffcd19c758d25760c0
shadon2024 <in-con23@yandex.ru>
2024-04-22 09:25:52 +0500

    new file info and new file user

---
+++ test/test/Info.swift
@@ -0,0 +1,11 @@
+//
+//  Info.swift
+//  test
+//
+//  Created by Admin on 22/04/24.
+//
+
+import Foundation
+import UIKit
+
+var info = "info"
---
+++ test/test/User.swift
@@ -0,0 +1,11 @@
+//
+//  User.swift
+//  test
+//
+//  Created by Admin on 22/04/24.
+//
+
+import Foundation
+import UIKit
+
+var user = "user"
